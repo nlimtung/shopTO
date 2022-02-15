c85e7cc3ed0fbd64eec7713c699d20714d6b4ddb
Ashu Bhanot <ashutoshbhanot@gmail.com>
2022-02-14 20:58:36 -0500

    replaced USER name in settings.py with .env variable - please update in your .env file

--- businesscollector/settings.py
+++ businesscollector/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'businesscollector',
-        'USER': 'postgres',
+        'USER': os.environ['USER'], #'USER': 'postgres',
         'PASSWORD': os.environ['PASSWORD'],
         'HOST': os.environ['HOST'],
         'PORT': os.environ['PORT'],
