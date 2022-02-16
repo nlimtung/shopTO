35828fa337c3602cdc6da26a41e385fda20eefcc
Nicole <nicole@Nicoles-MacBook-Air.local>
2022-02-15 19:41:53 -0500

    adding model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Business
 
+admin.site.register(Business)
 # Register your models here.
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+CATEGORY = (
+    ('Other', 'Other'),
+    ('Home', 'Home'),
+    ('Books', 'Books'),
+    ('Clothes', 'Clothes')
+)
+
+class Business(models.Model):
+    name = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 100)
+    city = models.CharField(max_length = 100)
+    province = models.CharField(max_length = 100)
+    postal_code = models.CharField(max_length = 100)
+    website = models.URLField(max_length = 200)
+    category = models.CharField(
+        max_length=10,
+        choices=CATEGORY,
+        default=CATEGORY[0][0]
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
