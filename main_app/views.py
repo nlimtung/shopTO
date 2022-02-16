aaa83fcce7c566d5379c0f9b3302cf6960eb77f2
Ashu Bhanot <ashutoshbhanot@gmail.com>
2022-02-15 20:26:25 -0500

    adding authentication feature

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 
 # Create your views here.
 def home (request):
     return render (request, 'home.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('/')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+#Create business
+# class CatCreate(LoginRequiredMixin, CreateView):
+# class CatCreate(CreateView):
+#   model = Cat
+#   fields = ['name', 'breed', 'description', 'age']
+  
+#   # This inherited method is called when a
+#   # valid cat form is being submitted
+#   def form_valid(self, form):
+#     # Assign the logged in user (self.request.user)
+#     form.instance.user = self.request.user  # form.instance is the cat
+#     # Let the CreateView do its job as usual
+#     return super().form_valid(form)
