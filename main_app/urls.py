5de2153b14ed20061eb54969b9196c34b669d5e9
Maha Abbas <maha.abbas9@gmail.com>
2022-02-16 18:37:07 -0600

    added changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+from django.urls import path
+from . import views
+
 urlpatterns =  [
     path ('', views.home, name = 'home'),
+    path('businesses/', views.businesses_index, name='index'),
+    path('businesses/<int:business_id>/', views.businesses_detail, name='detail'),
     path('accounts/signup/', views.signup, name='signup'),
     path('business/', views.business_index, name='index'),
     path('business/create/', views.BusinessCreate.as_view(), name='business_create'),
+    path ('category', views.category, name = 'category')
 ]
